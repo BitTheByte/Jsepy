7fce04fa1eb75eb6998c084b93ab8cf511336ced
Sourcery AI <>
2023-06-04 18:53:00 +0000

    'Refactored by Sourcery'

--- Jsepy/linkers/python.py
+++ Jsepy/linkers/python.py
@@ -1,8 +1,7 @@
 def handler(module,path,api_path):
 	target  = path.replace(api_path,"").split("/")
 	fn,args = target[0],target[1::]
-	fn = getattr(module, fn,None)
-	if fn:
+	if fn := getattr(module, fn, None):
 		try:
 			response = fn() if not args else fn(*args)
 		except Exception as e:
